c309dad336c1ce3556605bf1cab51ec4c0d06a63
dhia <dhiakacem2019@gmail.com>
2024-09-16 13:43:25 +0100

    Amazon Scrapping

--- html_scraper/amazon_scraper.py
+++ html_scraper/amazon_scraper.py
@@ -4,4 +4,8 @@ import bs4
 import csv
 
 if __name__ == "__main__":
-    print("hello")
+    with open('amazon_products_urls.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            url = row[0]
+            print(url)
